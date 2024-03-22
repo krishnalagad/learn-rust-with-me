0b69373272f1892be43ca25bf1d84bd034ab8f15
Krishna Lagad <krishnalagad2@gmail.com>
2024-03-22 13:18:00 +0530

    first project code in rust

--- first_project/src/main.rs
+++ first_project/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 10;
+    println!("Value of x: {}", x);
+    {
+        let x = 11 + x;
+        println!("Value of x: {}", x);
+    }
+    let x = 12;
+    println!("Value of x: {}", x);
+
+    const MINUTES: u32 = 59;
+    println!("Value of const var: {}", MINUTES);
+
+    
 }
