ab41f3bf877291a81df06a9ab1f6d924142148c7
Krishna Lagad <krishnalagad2@gmail.com>
2024-03-26 14:41:34 +0530

    commit pending files

--- compound_data_types.rs
+++ compound_data_types.rs
@@ -1,4 +1,15 @@
+/*
+    rustc compound_data_types.rs -o app && time ./app && rm app
+*/
 fn main() {
-    let tup: (i8, bool, char) = (9, true, 'K);
+    let tup: (i8, bool, char) = (9, true, 'K');
     println!("{}", tup.0);
+    println!("{}", tup.1);
+    println!("{}", tup.2);
+
+    let tup = ("krishna", 78, 'K', true);
+    println!("{}", tup.0);
+    println!("{}", tup.1);
+    println!("{}", tup.2);
+    println!("{}", tup.3);
 }
