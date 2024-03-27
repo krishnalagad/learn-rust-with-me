8d2d9e06dc3529527903b95c5c67183a39c4ea24
Krishna Lagad <krishnalagad2@gmail.com>
2024-03-27 16:30:58 +0530

    add rand depedency to generate random number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please enter a number: ");
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line!!");
+
+    println!("You guessed: {}", guess);
 }
