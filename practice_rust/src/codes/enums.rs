be2a4b015ff294610b947df98c4c97728d6b8eaf
Krishna Lagad <krishnalagad2@gmail.com>
2024-03-30 11:32:54 +0530

    enums in rust

--- practice_rust/src/codes/enums.rs
+++ practice_rust/src/codes/enums.rs
@@ -4,6 +4,20 @@ enum IpAddrType {
     IPV6(String)
 }
 
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move{x: u32, y: u32},
+    Write(String),
+    ChangeColor(i32, i32, i32)
+}
+
+impl Message {
+    fn some_function() {
+        println!("Body of some_function!!");
+    }
+}
+
 /*
     rustc enums.rs -o app && time ./app && rm app
  */
@@ -13,4 +27,8 @@ fn main() {
 
     let localhost_ipv6 = IpAddrType::IPV6(String::from("1234:4566:9876:9087"));
     println!("{:?}", localhost_ipv6);
+
+    let message = Message::Write(String::from("Black"));
+    println!("{:?}", message);
+    Message::some_function();
 }
