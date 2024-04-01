bea69b0c5d9fe3a51acad6101d6b146915930665
Krishna Lagad <krishnalagad2@gmail.com>
2024-04-01 11:20:37 +0530

    modules in rust

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,14 +1,21 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_wishlist() {}
+        pub fn seat_at_table() {}
+    }
+    pub mod serving {
+        pub fn take_order() { println!("Take order!!"); }
+        pub fn serve_order() {}
+        pub fn take_payment() {}
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn go_to_restaurant() {
+    front_of_house::serving::take_payment();
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub mod food {
+    fn eat_food() {
+        super::go_to_restaurant();  // we can access function in parent module using super keyword.
     }
 }
