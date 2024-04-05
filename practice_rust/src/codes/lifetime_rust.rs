a9036eb7ffce71dadf063dec150dbe210d9396fd
Krishna Lagad <krishnalagad2@gmail.com>
2024-04-05 14:02:24 +0530

    initial commit of the day

--- practice_rust/src/codes/lifetime_rust.rs
+++ practice_rust/src/codes/lifetime_rust.rs
@@ -8,7 +8,6 @@ fn main() {
     let result = longest(str1.as_str(), str2.as_str());
     println!("Longest string: {}", result);
 }
-
 fn longest<'a>(str1: &'a str, str2: &'a str) -> &'a str {
     if str1.len() > str2.len() {
         str1
