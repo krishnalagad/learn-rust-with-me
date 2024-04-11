1b303d7b5e1fda21c428d912acb8d82fe1c15bef
Krishna Lagad <krishnalagad2@gmail.com>
2024-04-11 13:02:58 +0530

    reference count smart pointer in rust

---
+++ practice_rust/src/codes/smart_pointer_ref_counting.rs
@@ -0,0 +1,25 @@
+use std::rc::Rc;
+#[derive(Debug)]
+enum List {
+    Con(i32, Rc<List>),
+    Nil
+}
+
+use List::{Con, Nil};
+
+/*
+    rustc smart_pointer_ref_counting.rs -o app && time ./app && rm app
+ */
+fn main() {
+    let a = Rc::new(List::Con(10, Rc::new(List::Con(20, Rc::new(List::Nil)))));
+    println!("Reference count of a: {}", Rc::strong_count(&a));
+
+    let b = List::Con(2, Rc::clone(&a));
+    println!("Reference count of a: {}", Rc::strong_count(&a));
+
+    {
+        let c = List::Con(5, Rc::clone(&a));
+        println!("Reference count of a in inner scope: {}", Rc::strong_count(&a));
+    }
+    println!("Final reference count of a: {}", Rc::strong_count(&a));
+}
